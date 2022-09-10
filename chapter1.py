95ff73038e2c56304330923d8736fe869fff4310
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-10 23:24:23 +0530

    resizing image

--- chapter1.py
+++ chapter1.py
@@ -2,18 +2,15 @@ import cv2
 import numpy as np
 
 img = cv2.imread("Resources/SampleImage.jpg")
-kernal = np.ones((5,5),np.uint8)
+print(img.shape)     #prints the width and height of the image along with its no of channels
 
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)    #blur should be in odd numbers like (7,7),(5,5),(3,3)
-imgCanny = cv2.Canny(img,100,100)
-imgDialation = cv2.dilate(imgCanny,kernal,iterations=1)  #useful for detecting disconnected edges
-imgEroded = cv2.erode(imgDialation,kernal,iterations=1)  #it is used to thin the edges.This does opposite of image Dialation
 
-cv2.imshow("Gray Image",imgGray)
-cv2.imshow("Blur Image",imgBlur)
-cv2.imshow("Canny Image",imgCanny)
-cv2.imshow("Dialation Imgae",imgDialation)
-cv2.imshow("Eroded Image",imgEroded)
+imgResize = cv2.resize(img, (300, 150))
+print(imgResize.shape)     #prints the width and height of the image along with its no of channels
+
+
+cv2.imshow("Image",img)
+cv2.imshow("Resized Image",imgResize)
+
 
 cv2.waitKey(0)
