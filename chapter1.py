74dbe1136cfc3eebd81ea4a84567b0b8f18aa999
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-10 23:36:08 +0530

    Cropping image

--- chapter1.py
+++ chapter1.py
@@ -8,9 +8,12 @@ print(img.shape)     #prints the width and height of the image along with its no
 imgResize = cv2.resize(img, (300, 150))
 print(imgResize.shape)     #prints the width and height of the image along with its no of channels
 
+imgCropped = img[0:200,100:200]
+
 
 cv2.imshow("Image",img)
 cv2.imshow("Resized Image",imgResize)
+cv2.imshow("Cropped Image",imgCropped)
 
 
 cv2.waitKey(0)
