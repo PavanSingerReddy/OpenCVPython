d61be00f47b038231a83426921af32305bed8026
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-10 22:58:35 +0530

    converting canny image to dialation image sixth commit

--- chapter1.py
+++ chapter1.py
@@ -1,13 +1,17 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("Resources/SampleImage.jpg")
+kernal = np.ones((5,5),np.uint8)
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)    #blur should be in odd numbers like (7,7),(5,5),(3,3)
 imgCanny = cv2.Canny(img,100,100)
+imgDialation = cv2.dilate(imgCanny,kernal,iterations=1)  #useful for detecting disconnected edges
 
 cv2.imshow("Gray Image",imgGray)
 cv2.imshow("Blur Image",imgBlur)
-cv2.imshow("canny image",imgCanny)
+cv2.imshow("Canny Image",imgCanny)
+cv2.imshow("Dialation Imgae",imgDialation)
 
 cv2.waitKey(0)
