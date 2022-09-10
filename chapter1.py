08293198dbc4ad7ef300765e19706e244efdd3d6
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-10 22:17:21 +0530

    converting Gray scale image to Blur image fifth commit

--- chapter1.py
+++ chapter1.py
@@ -3,7 +3,9 @@ import cv2
 img = cv2.imread("Resources/pic1.jpg")
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)    #blur should be in odd numbers like (7,7),(5,5),(3,3)
 
-cv2.imshow("Gray Imgae",imgGray)
+cv2.imshow("Gray Image",imgGray)
+cv2.imshow("Blur Image",imgBlur)
 
 cv2.waitKey(0)
