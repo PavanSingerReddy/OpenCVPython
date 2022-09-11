ca4350c96550c66e04369f3042d2816d854b3392
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-11 14:33:36 +0530

    writing a text on image

--- chapter1.py
+++ chapter1.py
@@ -7,7 +7,8 @@ img = np.zeros((512,512,3),np.uint8)
 
 # cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
 # cv2.rectangle(img,(0,0),(250,350),(0,255,0),cv2.FILLED)
-cv2.circle(img,(300,100),70,(255,255,0),3)
+# cv2.circle(img,(300,100),70,(255,255,0),3)
+cv2.putText(img,"OPENCV ",(300,100),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
 
 
 cv2.imshow("Image",img)
