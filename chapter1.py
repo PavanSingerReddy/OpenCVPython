b062c9c132eb11504e6cbb4c40388f0e573d6c12
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-11 14:02:16 +0530

    grey scale black image creation

--- chapter1.py
+++ chapter1.py
@@ -1,19 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("Resources/SampleImage.jpg")
-print(img.shape)     #prints the width and height of the image along with its no of channels
-
-
-imgResize = cv2.resize(img, (300, 150))
-print(imgResize.shape)     #prints the width and height of the image along with its no of channels
-
-imgCropped = img[0:200,100:200]
+img = np.zeros((512,512))
 
 
 cv2.imshow("Image",img)
-cv2.imshow("Resized Image",imgResize)
-cv2.imshow("Cropped Image",imgCropped)
 
 
 cv2.waitKey(0)
