db8c3c5dba90229ef12362109b7ee1aeab958867
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-11 15:20:07 +0530

    Wrap Perspective

--- chapter1.py
+++ chapter1.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-img = np.zeros((512,512,3),np.uint8)
-# print(img.shape)
-# img[:] = 255,0,0
+img = cv2.imread("Resources/cards.jpg")
 
-# cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
-# cv2.rectangle(img,(0,0),(250,350),(0,255,0),cv2.FILLED)
-# cv2.circle(img,(300,100),70,(255,255,0),3)
-cv2.putText(img,"OPENCV ",(300,100),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
+width,height = 250,350
 
+pts1 = np.float32([[498,447],[1407,351],[666,1743],[1647,1557]])        #actual dimensions of the single card in the entire image use gimp editor to get the dimensions
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])          #resized dimension or wrap dimension points
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img,matrix,(width,height))
 
-cv2.imshow("Image",img)
+cv2.imshow("image",img)
+cv2.imshow("Cards image",imgOutput)
 
 
 cv2.waitKey(0)
