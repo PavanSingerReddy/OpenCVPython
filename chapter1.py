47c6971436fb057633f964e06a9dc1a6e7747945
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-11 14:20:03 +0530

    drawing a circle

--- chapter1.py
+++ chapter1.py
@@ -6,7 +6,9 @@ img = np.zeros((512,512,3),np.uint8)
 # img[:] = 255,0,0
 
 # cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
-cv2.rectangle(img,(0,0),(250,350),(0,255,0),cv2.FILLED)
+# cv2.rectangle(img,(0,0),(250,350),(0,255,0),cv2.FILLED)
+cv2.circle(img,(300,100),70,(255,255,0),3)
+
 
 cv2.imshow("Image",img)
 
