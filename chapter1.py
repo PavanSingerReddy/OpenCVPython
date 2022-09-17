e719e8d7abe9c58232a46c255bf366864d5b406f
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-17 20:17:24 +0530

    blurring the image and combining them for shape detection

--- chapter1.py
+++ chapter1.py
@@ -1,11 +1,6 @@
 import cv2
 import numpy as np
 
-def empty(a):
-    pass
-
-
-
 def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -39,44 +34,18 @@ def stackImages(scale,imgArray):
 
 
 
-
-path = "Resources/cars.jpeg"
-cv2.namedWindow("TrackBars")
-cv2.resizeWindow("TrackBars",640,240)
-cv2.createTrackbar("Hue Min","TrackBars",85,179,empty)
-cv2.createTrackbar("Hue Max","TrackBars",179,179,empty)
-cv2.createTrackbar("Sat Min","TrackBars",8,255,empty)
-cv2.createTrackbar("Sat Max","TrackBars",255,255,empty)
-cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
-cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+path = 'Resources/shapes.png'
+img = cv2.imread(path)
 
 
-while True:
-
-    img = cv2.imread(path)
-
-    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
-    h_min =cv2.getTrackbarPos("Hue Min","TrackBars")
-    h_max =cv2.getTrackbarPos("Hue Max","TrackBars")
-    s_min =cv2.getTrackbarPos("Sat Min","TrackBars")
-    s_max =cv2.getTrackbarPos("Sat Max","TrackBars")
-    v_min =cv2.getTrackbarPos("Val Min","TrackBars")
-    v_max =cv2.getTrackbarPos("Val Max","TrackBars")
-
-    print(h_min,h_max,s_min,s_max,v_min,v_max)
-
-    lower = np.array([h_min,s_min,v_min])
-    upper = np.array([h_max,s_max,v_max])
-
-    mask =cv2.inRange(imgHSV,lower,upper)
-
-    imgResult = cv2.bitwise_and(img,img,mask=mask)
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
 
 
-    imgStack = stackImages(0.5,([img,imgHSV],[mask,imgResult]))
+
+imgStack = stackImages(1,([img,imgGray,imgBlur]))
 
 
-    cv2.imshow("Stacked Images",imgStack)
+cv2.imshow("Stack",imgStack)
 
-    cv2.waitKey(1)
+cv2.waitKey(0)
