cffc792f3e1b282a7f8b596f12df866412affc03
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-17 15:08:08 +0530

    tracking multiple trackbars

--- chapter1.py
+++ chapter1.py
@@ -17,15 +17,23 @@ cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
 cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
 
 
+while True:
 
-img = cv2.imread(path)
+    img = cv2.imread(path)
 
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    h_min =cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max =cv2.getTrackbarPos("Hue Max","TrackBars")
+    s_min =cv2.getTrackbarPos("Sat Min","TrackBars")
+    s_max =cv2.getTrackbarPos("Sat Max","TrackBars")
+    v_min =cv2.getTrackbarPos("Val Min","TrackBars")
+    v_max =cv2.getTrackbarPos("Val Max","TrackBars")
 
+    print(h_min)
 
-cv2.imshow("Original",img)
+    cv2.imshow("Original",img)
 
-cv2.imshow("HSV",imgHSV)
+    cv2.imshow("HSV",imgHSV)
 
-cv2.waitKey(0)
+    cv2.waitKey(1)
