e2af2c58174c17534836dd21e57e50f1ae508c4e
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-17 23:22:33 +0530

    detecting faces and drawing bounding boxes

--- chapter1.py
+++ chapter1.py
@@ -1,81 +1,17 @@
 import cv2
 import numpy as np
 
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
-
-
-
-def getContors(img) :
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        print(area)
-        if area > 500:
-            cv2.drawContours(imgContour,cnt,-1,(255,0,0),3)
-            peri = cv2.arcLength(cnt,closed=True)
-            # print(peri)
-            approx = cv2.approxPolyDP(cnt,0.03*peri,True)
-            print(len(approx))
-            objCorners = len(approx)
-            x,y,width,height = cv2.boundingRect(approx)
-
-            if objCorners == 3 : objectType = "Triangle"
-            elif objCorners == 4 :
-                aspRatio = width/float(height)
-                if aspRatio > 0.95 and aspRatio < 1.05 : objectType = "Square"
-                else:objectType = "Rectangle"
-            elif objCorners == 5 : objectType = "Pentagon"
-            elif objCorners == 6 : objectType = "Hexagon"
-            elif objCorners > 6 : objectType = "Circle"
-            else:objectType = "None"
-
-            cv2.rectangle(imgContour,(x,y),(x+width,y+height),(0,255,0),2)
-            cv2.putText(imgContour,objectType,(x+(width//2)-10,y+(height//2)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
-
-path = 'Resources/shapes.png'
-img = cv2.imread(path)
-imgContour = img.copy()
-
-
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
+img = cv2.imread("Resources/SampleImage.jpg")
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
-imgCanny = cv2.Canny(imgBlur,50,50)
-getContors(imgCanny)
-
-imgBlank = np.zeros_like(img)      #normal blank image this is not required we have took it to stack the images
-
-imgStack = stackImages(1,([img,imgGray,imgBlur],[imgCanny,imgContour,imgBlank]))
 
 
-cv2.imshow("Stack",imgStack)
 
+faces = faceCascade.detectMultiScale(imgGray,1.1,4)
+
+for (x,y,width,height) in faces:
+    cv2.rectangle(img,(x,y),(x+width,y+height),(255,0,0),2)
+
+
+cv2.imshow("Result",img)
 cv2.waitKey(0)
