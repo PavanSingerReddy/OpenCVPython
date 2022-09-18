9579fa4524b994ad972cbf0c754b8e9948d59c12
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-18 20:31:40 +0530

    getting contours and returning the biggest contour in project 2

--- project_2 - Document Scanner.py
+++ project_2 - Document Scanner.py
@@ -21,12 +21,27 @@ def preProcessing(img):
 
     return imgThreshold
 
-
+def getContours(img):
+    biggest =  np.array([])
+    maxArea = 0
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours :
+        area = cv2.contourArea(cnt)
+        if area >5000:
+            cv2.drawContours(imgCountours,cnt,-1,(255,0,0),3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.03*peri,True)
+            if area > maxArea and len(approx) ==4:
+                biggest = approx
+                maxArea = area
+    return biggest
 
 while True:
     success , img = cap.read()
     img = cv2.resize(img,(widthImg,heightImg))
+    imgCountours = img.copy()
     imgThreshold = preProcessing(img)
-    cv2.imshow("Result",imgThreshold)
+    getContours(imgThreshold)
+    cv2.imshow("Result",imgCountours)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
