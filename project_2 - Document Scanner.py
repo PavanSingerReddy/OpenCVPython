78946ebd0d2188a4754cc7b9a6badd43f8422db4
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-18 19:35:19 +0530

    preprossing image in project 2

--- project_2 - Document Scanner.py
+++ project_2 - Document Scanner.py
@@ -1,15 +1,32 @@
 import cv2
+import numpy as np
 
-frameWidth = 640
-frameHeight = 480
+###########################
+widthImg = 640
+heightImg = 480
+###########################
 
 cap =  cv2.VideoCapture(0)
-cap.set(3,frameWidth)
-cap.set(4,frameHeight)
+cap.set(3,widthImg)
+cap.set(4,heightImg)
 cap.set(10,130)
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny = cv2.Canny(imgBlur,200,200)
+    kernal = np.ones((5,5))
+    imgDial = cv2.dilate(imgCanny,kernal,iterations=2)
+    imgThreshold = cv2.erode(imgDial,kernal,iterations=1)      #eroding the dialated image
+
+    return imgThreshold
+
+
 
 while True:
     success , img = cap.read()
-    cv2.imshow("Result",img)
+    img = cv2.resize(img,(widthImg,heightImg))
+    imgThreshold = preProcessing(img)
+    cv2.imshow("Result",imgThreshold)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
