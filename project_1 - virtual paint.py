a7cbc5af99c1a4e949728d2a47801cebbfdd7a43
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-18 16:59:57 +0530

    detecting all color points in virtual paint

--- project_1 - virtual paint.py
+++ project_1 - virtual paint.py
@@ -12,15 +12,18 @@ cap.set(10,130)
 
 myColors = [[88,102,58,255,159,255]]
 
+myColorValues = [[255,0,0]]
+
 def findColor(img,myColors):
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    count = 0
     for color in myColors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV,lower,upper)
         x,y=getContours(mask)
-        cv2.circle(imgResult,(x,y),10,(255,0,0),cv2.FILLED)
-
+        cv2.circle(imgResult,(x,y),10,myColorValues[count],cv2.FILLED)
+        count +=1
         # cv2.imshow(str(color[0]),mask)
 
 
@@ -30,7 +33,7 @@ def getContours(img):
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 500:
-            cv2.drawContours(imgResult,cnt,-1,(255.0,0),3)
+            # cv2.drawContours(imgResult,cnt,-1,(255.0,0),3)
             peri=cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.03*peri,True)
             x,y,width,height = cv2.boundingRect(approx)
