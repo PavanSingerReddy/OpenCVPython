90e398f56cb6cfd2a138360efeb02d7b65921aad
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-09-18 17:59:59 +0530

    drawing lines on detected points in virtual paint and project 1 is completed

--- project_1 - virtual paint.py
+++ project_1 - virtual paint.py
@@ -12,19 +12,25 @@ cap.set(10,130)
 
 myColors = [[88,102,58,255,159,255]]
 
-myColorValues = [[255,0,0]]
+myColorValues = [[255,0,0]]         #BGR
+
+myPoints = []        #[x,y,ColorId]
 
 def findColor(img,myColors):
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     count = 0
+    newPoints=[]
     for color in myColors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV,lower,upper)
         x,y=getContours(mask)
-        cv2.circle(imgResult,(x,y),10,myColorValues[count],cv2.FILLED)
-        count +=1
+        if x!=0 and y!=0:
+            cv2.circle(imgResult, (x, y), 10, myColorValues[count], cv2.FILLED)
+            newPoints.append([x,y,count])
+            count +=1
         # cv2.imshow(str(color[0]),mask)
+    return newPoints
 
 
 def getContours(img):
@@ -39,10 +45,21 @@ def getContours(img):
             x,y,width,height = cv2.boundingRect(approx)
     return x+(width//2),y
 
+
+def drawOnCanvas(myPoints,myColorValues) :
+    for point in myPoints :
+        cv2.circle(imgResult,(point[0],point[1]),10,myColorValues[point[2]],cv2.FILLED)  #point[0] is x value point[1] is y value and point[2] is colorId value
+
 while True:
     success,img = cap.read()
     imgResult = img.copy()
-    findColor(img,myColors)
+    newPoints =  findColor(img,myColors)
+    if len(newPoints)!=0:
+        for newP in newPoints:
+            myPoints.append(newP)
+
+    if len(myPoints)!=0:
+        drawOnCanvas(myPoints,myColorValues)
     cv2.imshow("Result",imgResult)
     if cv2.waitKey(1)&0xFF == ord('q'):
         break
